9eda91d9bbef1fb715af04523fb71f1c66494d6f
Aadityavv <aadityavv9@gmail.com>
2024-07-07 17:08:06 +0530

    dynamic values added

--- index.js
+++ index.js
@@ -11,21 +11,41 @@ const openweatherKey="62fb0753edefa3493431dc7cfa992c7b";
 app.get("/",async(req,res)=>{
     const getLocation= await axios.get("http://ip-api.com/json/")
     console.log(getLocation.data.city);
-    res.render("index.ejs",{
-        city:getLocation.data.city,
-        country:getLocation.data.country
-    })
-})
-
-app.post("/search", async(req,res)=>{
-    const city=req.body.search;
-    console.log(city);
+    const city=getLocation.data.city;
     const searchedLocation= await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openweatherKey}`);
-    console.log(searchedLocation);
+
+    
 
     res.render("index.ejs",{
         city:searchedLocation.data.name,
-        country: searchedLocation.data.sys.country
+        country: searchedLocation.data.sys.country,
+        expectations:searchedLocation.data.weather[0].main,
+        temp:Math.floor((searchedLocation.data.main.temp)-273.15),
+        icon:searchedLocation.data.weather[0].icon,
+        realTemp:Math.floor((searchedLocation.data.main.feels_like)-273.15),
+        wind:searchedLocation.data.wind.speed,
+        chancesRain:searchedLocation.data.rain["1h"],
+        humidity:searchedLocation.data.main.humidity
+    })        
+    })
+
+app.post("/search", async(req,res)=>{
+
+    const city=req.body.search;
+    console.log(city);
+
+    const searchedLocation= await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openweatherKey}`);
+
+    res.render("index.ejs",{
+        city:searchedLocation.data.name,
+        country: searchedLocation.data.sys.country,
+        expectations:searchedLocation.data.weather[0].main,
+        temp:Math.floor((searchedLocation.data.main.temp)-273.15),
+        icon:searchedLocation.data.weather[0].icon,
+        realTemp:Math.floor((searchedLocation.data.main.feels_like)-273.15),
+        wind:searchedLocation.data.wind.speed,
+        chancesRain:searchedLocation.data.rain["1h"],
+        humidity:searchedLocation.data.main.humidity
     })
 })
 
